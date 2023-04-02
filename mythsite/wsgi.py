711f53f1763c9a6317c0c47582d0146bcb7daef1
Ethan Ashkie <ethanashkie@gmail.com>
2023-04-02 13:55:09 -0700

    Rename site folder to mythsite

--- mythsite/wsgi.py
+++ mythsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for infadsite project.
+WSGI config for mythsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infadsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mythsite.settings')
 
 application = get_wsgi_application()
